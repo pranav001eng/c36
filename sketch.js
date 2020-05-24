2c84eb99778aac2cea4b426aded12086b77cdcf8
pranav001eng <63138681+pranav001eng@users.noreply.github.com>
2020-05-24 10:58:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas,backgroundImage;
+var gameState = 0;
+var playerCount;
+var database;
+var form,player,game;
+
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+
+}
+
+function draw(){
+
+}
+
+
